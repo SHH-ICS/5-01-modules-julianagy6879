2047fa84fccf3e068cd527a5be05bc543198f815
julianagy6879 <julia.nagy@stu.ocsb.ca>
2024-11-26 15:07:48 +0000

    changes

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,2 +1,6 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
+a = float(input("Enter value for leg a of triangle"))**2
+b = float(input("Enter value for leg b of triangle"))**2
+c = float(input(a + b))
+print("Here is your hypoteneuse!")
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,37 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import pygame
+
+from pygame import Color, Rect
+from pygame import draw
+from pygame import display
+
+SCREEN_SIZE = (500, 500)
+
+# initialize pygame modules
+pygame.init()
+
+# get a surface for graphics display
+gameDisplay = display.set_mode(SCREEN_SIZE)
+
+# background - color of the sky
+gameDisplay.fill(Color('lightblue'))
+
+# draw a house with a roof
+draw.rect(gameDisplay, Color('yellow'), Rect(200, 400, 600, 400))
+draw.polygon(gameDisplay, Color('orange'), [(300, 400), (800, 400), (300, 100)])
+
+# draw green grass
+draw.rect(gameDisplay, Color('purple'), Rect(600, 400, 500, 400))
+
+# draw a moon
+draw.circle(gameDisplay, Color('beige'), (100, 100), 300)
+
+# draw a sun
+draw.circle(gameDisplay, Color('black'), (250, 200), 50)
+
+# show the graphics on the screen
+display.flip()
+
+# Wait for user input before closing the window
+input("Press enter to exit")
